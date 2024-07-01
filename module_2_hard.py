e1994c992f05152f4416caef722550169fcabfd4
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-07-01 20:37:11 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,7 +1,10 @@
 n = int(input())
-print(f'{n}:', end='')
-for x1 in range(1, n):
-    for x2 in range(x1 + 1, n):
-        if n % (x1 + x2) == 0:
-            print(f'{x1}{x2}', end='')
+if n >= 3 and n <= 20:
+    print(f'{n}:', end='')
+    for x1 in range(1, n):
+        for x2 in range(x1 + 1, n):
+            if n % (x1 + x2) == 0:
+                print(f'{x1}{x2}', end='')
+else:
+    print("Число не верное")
 print()
